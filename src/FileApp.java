74f9e12ea9ba279da05ee2273db3a052bdcf4138
midnyt <theplashrz@gmail.com>
2023-03-02 04:34:13 +0530

    Completed all test cases

--- src/FileApp.java
+++ src/FileApp.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class FileApp {
 
-    static ArrayList<File> fileList = new ArrayList<>();
+    static List<File> fileList = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -16,19 +16,18 @@ public class FileApp {
 
         FileApp fa = new FileApp();
         fa.loadFiles();
-        //fa.chooseOperation();
-        fa.subOperation();
+        fa.chooseOperation();
 
     }
 
     private void chooseOperation() {
 
-        String[] options1 = {"Show current files in Locker.","Select a file from the Locker.","Close LockedMe"};
+        String[] options1 = {"Show current files in Locker.", "Select a file from the Locker.", "Close LockedMe"};
 
         System.out.println();
         System.out.println("Kindly select the desired operation:-\t\t|(eg.. 1 and hit enter!)");
-        for(int i=0;i<options1.length;i++){
-            System.out.println((i+1)+": "+options1[i]);
+        for (int i = 0; i < options1.length; i++) {
+            System.out.println((i + 1) + ": " + options1[i]);
         }
 
         switch (new Scanner(System.in).nextInt()) {
@@ -46,7 +45,7 @@ public class FileApp {
     private void displayFiles() {
         System.out.println();
         fileList.sort(Comparator.naturalOrder());
-        fileList.iterator().forEachRemaining(e-> System.out.println(e.getName()));
+        fileList.iterator().forEachRemaining(e -> System.out.println(e.getName()));
         System.out.println();
 
         chooseOperation();
@@ -55,7 +54,7 @@ public class FileApp {
     private void subOperation() {
 
         String[] options2 = {"Add a file to the Locker.", "Delete a file from the Locker.",
-                "Search a file from the Locker","Return to previous menu.", "Close LockedMe"};
+                "Search a file from the Locker", "Return to previous menu.", "Close LockedMe"};
 
         System.out.println();
         System.out.println("Kindly select the desired operation:-\t\t|(eg.. 1 and hit enter!)");
@@ -73,7 +72,6 @@ public class FileApp {
                 System.out.println("Incorrect option!...please choose wisely");
                 subOperation();
             }
-
         }
     }
 
@@ -87,35 +85,34 @@ public class FileApp {
         String name = caseCorrect(new Scanner(System.in).nextLine());
         String s;
         boolean flag = false;
-        int index=0;
+        int index = 0;
 
-        for(int i=0;i<fileList.size();i++){
+        for (int i = 0; i < fileList.size(); i++) {
 
-            if(fileList.get(i).getName().contains(".")){
-                s=fileList.get(i).getName().substring(0,fileList.get(i).getName().indexOf("."));
-            }else {
-                s=fileList.get(i).getName();
+            if (fileList.get(i).getName().contains(".")) {
+                s = fileList.get(i).getName().substring(0, fileList.get(i).getName().indexOf("."));
+            } else {
+                s = fileList.get(i).getName();
             }
-            if(s.equals(name)){
+            if (s.equals(name)) {
                 flag = true;
                 index = i;
                 break;
             }
         }
 
-        if(flag){
-            System.out.println(fileList.get(index).getName()+" is present at "+index+"th location!");
-        }else{
-            System.out.println("There is no such file or folder present by the name of: "+name);
+        if (flag) {
+            System.out.println(fileList.get(index).getName() + " is present at " + index + "th location!");
+        } else {
+            System.out.println("There is no such file or folder present by the name of: " + name);
         }
-
         subOperation();
     }
 
     private String caseCorrect(String name) {
 
         StringBuilder sb = new StringBuilder(name.toLowerCase());
-        sb.setCharAt(0,Character.toUpperCase(sb.charAt(0)));
+        sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
         //sb.append(".txt");
 
         return sb.toString();
@@ -123,6 +120,48 @@ public class FileApp {
 
     private void deleteFile() {
 
+        System.out.println("Enter the name of the file: \t"+
+                "Eg. abc.txt for File\t" +
+                "& abc for Directory");
+        System.out.println();
+        String fname = caseCorrect(new Scanner(System.in).nextLine());
+        String strPattern = "[^a-zA-Z0-9_.]";
+        Pattern p = Pattern.compile(strPattern);
+        Matcher m = p.matcher(fname);
+        boolean chk = false;
+
+        if (m.find()) {
+
+            System.out.println("Files are created with a strict naming convention." +
+                    "\nthere are no special character files. Try deleting something that actually exists");
+
+        } else {
+
+            for (int i = 0; i < fileList.size(); i++) {
+
+                if (fileList.get(i).getName().equals(fname)) {
+                    File dFile = new File("src/FilesLockerRoom/" + fname);
+                    if (dFile.delete()) {
+
+                        if(fileList.get(i).isDirectory()){
+                            System.out.println("Directory Deleted");
+                        } else {
+                            System.out.println("File Deleted");
+                        }
+                        fileList.remove(i);
+                        chk = true;
+                        break;
+
+                    } else {
+                        System.out.println("Unable to delete the File/Directory");
+                    }
+                }
+            }
+            if (!chk) {
+                System.out.println("No such file/directory present. Check the list again!");
+            }
+        }
+        subOperation();
     }
 
 
@@ -134,7 +173,7 @@ public class FileApp {
         Pattern p = Pattern.compile(strPattern);
         Matcher m = p.matcher(fname);
 
-        if(m.find()){
+        if (m.find()) {
 
             System.out.println("!!No special characters or spaces are allowed");
             System.out.println("underscores (_) and numbers are eligible");
@@ -157,7 +196,7 @@ public class FileApp {
             }
 
         }
-
+        subOperation();
     }
 
     private void exitOnclose() {

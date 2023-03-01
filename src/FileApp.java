445c7ad9a9bcec828749a736b0a5bca65466efbe
midnyt <theplashrz@gmail.com>
2023-03-01 17:38:32 +0530

    added files

--- src/FileApp.java
+++ src/FileApp.java
@@ -2,8 +2,11 @@ public class FileApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome!!");
-        System.out.println("testing git");
+        System.out.println("===========================================================");
+        System.out.println("\t\t\tWelcome to Lockers Pvt. Ltd.\t\t\n\t\t\t\t\tLockedMe.com");
+        System.out.println("===========================================================");
+
+        String[] options = {"Add a file to the Locker.","Delete a file from the Locker.","Search a file from the Locker"};
 
     }
 
